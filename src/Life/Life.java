4cb6c85b85c4523e34718c8324afccc39e647099
Miner-code <hugovallet7@gmail.com>
2024-04-25 12:13:24 +0200

    life structure + creature in coming

---
+++ src/Life/Life.java
@@ -0,0 +1,20 @@
+package Life;
+
+abstract class Life {
+    public String name;
+    public boolean sex;
+    public int age;
+    public String species;
+
+    public Life (String name, boolean sex, int age, String species) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.species = species;
+    }
+
+    abstract void scream();
+    abstract void gettingOld();
+    abstract void sleeping();
+
+}
