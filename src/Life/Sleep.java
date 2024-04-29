ecff0d32c1607adf6d01d2b77228ad5b2d32a426
Loukas Campanale <loukascampanale125@gmail.com>
2024-04-29 09:55:38 +0200

    lala

---
+++ src/Life/Sleep.java
@@ -0,0 +1,12 @@
+package Life;
+
+abstract class Sleep {
+
+    public boolean sleep;
+    public int lastSleep;
+    public int needSleep;
+
+    public static void main(String[] args) {
+
+    }
+}
