402f09211446a17b3ffa20dd9c0f1ec582da6ee6
Warzaros <62097319+Miner-code@users.noreply.github.com>
2024-05-28 19:34:06 +0200

    Update Aviary.java

--- src/Zoo/Enclosure/Aviary.java
+++ src/Zoo/Enclosure/Aviary.java
@@ -1,10 +1,14 @@
 package Zoo.Enclosure;
 
-public class Aviary extends Zoo.Enclosure.Enclosure {
+import Zoo.Creature.Creature;
+import Zoo.Creature.Dragon;
+import Zoo.Creature.Phoenix;
+
+public class Aviary extends Enclosure {
     double height;
 
-    public Aviary(String name, double area, int maxNumberOfCreatures, double height) {
-        super(name, area, maxNumberOfCreatures);
+    public Aviary(String name, int size, double height) {
+        super(name, size);
         this.height = height;
     }
 
@@ -12,5 +16,15 @@ public class Aviary extends Zoo.Enclosure.Enclosure {
     public void displayCharacteristics() {
         super.displayCharacteristics();
         System.out.println("Height: " + this.height);
+    }
+
+    @Override
+    public boolean addCreature(Creature creature) {
+        if (creature instanceof Dragon || creature instanceof Phoenix) {
+            return super.addCreature(creature);
+        } else {
+            System.out.println("This type of creature cannot be added to this aviary.");
+            return false;
+        }
     }
 }
